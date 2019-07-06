09c5aea5a5e8ac36a57a1cba34326ece5116d6c2
Aleksei Bekh-Ivanov <bekh6ex@users.noreply.github.com>
2019-07-06 14:14:32 +0200

    Encapsulate ProxyListModel

--- src/options/ProxyList.js
+++ src/options/ProxyList.js
@@ -1,39 +1,44 @@
-const ProxyListModel = {
-    list: [],
-    loadList: async () => {
-        ProxyListModel.list = await store.getAllProxies()
-        m.redraw()
-    },
-    current: {},
-    delete: async (id) => {
-        await store.deleteProxyById(id)
-        await ProxyListModel.loadList()
+const ProxyList = (()=>{
+    
+    const ProxyListModel = {
+        list: [],
+        loadList: async () => {
+            ProxyListModel.list = await store.getAllProxies()
+            m.redraw()
+        },
+        current: {},
+        delete: async (id) => {
+            await store.deleteProxyById(id)
+            await ProxyListModel.loadList()
+        }
     }
-}
 
-function renderProxyItem(p) {
-    const text = p.title ? p.title : `${p.host}:${p.port}`;
-    const editButton = m('button.edit[type=button]', {
-        href: '/proxies/' + p.id,
-        oncreate: m.route.link,
-        class:  style.action
-    }, 'edit');
-    const deleteButton = m('button.delete[type=button]', {
-        oncreate: m.route.link,
-        onclick: () => ProxyListModel.delete(p.id),
-        class: style.action
-    }, 'delete');
-    return m('.proxy-list-item', [text, editButton, deleteButton])
-}
-
-
-const ProxyList = {
-    oninit: ProxyListModel.loadList,
-    view: () => {
-        const items = ProxyListModel.list.map(renderProxyItem);
-        const actions = m('.actions', [
-            m('a[href=/proxies/new]', {oncreate: m.route.link, class: style.action }, "+")
-        ])
-        return [...items, actions]
+    function renderProxyItem(p) {
+        const text = p.title ? p.title : `${p.host}:${p.port}`;
+        const editButton = m('button.edit[type=button]', {
+            href: '/proxies/' + p.id,
+            oncreate: m.route.link,
+            class:  style.action
+        }, 'edit');
+        const deleteButton = m('button.delete[type=button]', {
+            oncreate: m.route.link,
+            onclick: () => ProxyListModel.delete(p.id),
+            class: style.action
+        }, 'delete');
+        return m('.proxy-list-item', [text, editButton, deleteButton])
     }
-}
+
+
+    const ProxyList = {
+        oninit: ProxyListModel.loadList,
+        view: () => {
+            const items = ProxyListModel.list.map(renderProxyItem);
+            const actions = m('.actions', [
+                m('a[href=/proxies/new]', {oncreate: m.route.link, class: style.action }, "+")
+            ])
+            return [...items, actions]
+        }
+    }
+    
+    return ProxyList
+})()
