a92f687a0fc53401ea814b629d86377c2efdc881
Aleksei Bekh-Ivanov <bekh6ex@users.noreply.github.com>
2019-07-06 14:16:41 +0200

    Encapsulate ContainerListModel

--- src/options/ContainerListView.js
+++ src/options/ContainerListView.js
@@ -1,61 +1,72 @@
-const ContainerListModel = {
-    containers: [],
-    proxies: [],
-    proxiesById: {},
-    relations: {},
-    loadAll: async () => {
-        ContainerListModel.containers = await browser.contextualIdentities.query({});
-        ContainerListModel.proxies = await store.getAllProxies()
-        ContainerListModel.proxies.forEach(p => ContainerListModel.proxiesById[p.id] = p )
-        const result = await browser.storage.local.get('relations');
-        ContainerListModel.relations = result.relations || {}
-        m.redraw()
-    },
-    saveRelations: async () => {
-        await browser.storage.local.set({relations: ContainerListModel.relations});
-    }
-};
+const ContainerListView = (() => {
 
-
-function renderSelectProxy(cookieStoreId, proxyId) {
-    const proxyOptions = ContainerListModel.proxies.map(p => m('option', {value: p.id, selected: p.id === proxyId}, p.title ? p.title : `${p.host}:${p.port}`));
-    const defaultOption = m('option', {value: '', selected: !proxyId}, '<from network settings>')
-    return m(
-        'select',
-        {
-            oninput: (e) => {
-                const proxyId = e.target.value
-                if (proxyId === '') {
-                    delete ContainerListModel.relations[cookieStoreId]
-                } else {
-                    ContainerListModel.relations[cookieStoreId] = [proxyId]
-                }
-                ContainerListModel.saveRelations()
-            }
+    const ContainerListModel = {
+        containers: [],
+        proxies: [],
+        proxiesById: {},
+        relations: {},
+        loadAll: async () => {
+            ContainerListModel.containers = await browser.contextualIdentities.query({});
+            ContainerListModel.proxies = await store.getAllProxies()
+            ContainerListModel.proxies.forEach(p => ContainerListModel.proxiesById[p.id] = p)
+            const result = await browser.storage.local.get('relations');
+            ContainerListModel.relations = result.relations || {}
+            m.redraw()
         },
-        [defaultOption, ...proxyOptions]
-    )
+        saveRelations: async () => {
+            await browser.storage.local.set({relations: ContainerListModel.relations});
+        }
+    };
 
-}
 
-function renderContainerItem(container) {
-    //{ name: "Personal", icon: "fingerprint", iconUrl: "resource://usercontext-content/fingerprint.svg", color: "blue", colorCode: "#37adff", cookieStoreId: "firefox-container-1" }
+    function renderSelectProxy(cookieStoreId, proxyId) {
+        const proxyOptions = ContainerListModel.proxies.map(p => m('option', {
+            value: p.id,
+            selected: p.id === proxyId
+        }, p.title ? p.title : `${p.host}:${p.port}`));
+        const defaultOption = m('option', {value: '', selected: !proxyId}, '<from network settings>')
+        return m(
+            'select',
+            {
+                oninput: (e) => {
+                    const proxyId = e.target.value
+                    if (proxyId === '') {
+                        delete ContainerListModel.relations[cookieStoreId]
+                    } else {
+                        ContainerListModel.relations[cookieStoreId] = [proxyId]
+                    }
+                    ContainerListModel.saveRelations()
+                }
+            },
+            [defaultOption, ...proxyOptions]
+        )
 
-    const proxies = ContainerListModel.relations[container.cookieStoreId] || [];
-
-    return m('.container-item', [
-        m('.container-name', container.name),
-        m('.attached-proxies', [
-            renderSelectProxy(container.cookieStoreId,proxies[0])
-        ]),
-    ])
-}
-
-const ContainerListView = {
-    oninit: ContainerListModel.loadAll,
-    view: () => {
-        const items = ContainerListModel.containers.map(renderContainerItem);
-        const defaultContainer = renderContainerItem({cookieStoreId:'firefox-default', name: '<default container>' })
-        return m('.containers', [...items ,defaultContainer])
     }
-}
+
+    function renderContainerItem(container) {
+        //{ name: "Personal", icon: "fingerprint", iconUrl: "resource://usercontext-content/fingerprint.svg", color: "blue", colorCode: "#37adff", cookieStoreId: "firefox-container-1" }
+
+        const proxies = ContainerListModel.relations[container.cookieStoreId] || [];
+
+        return m('.container-item', [
+            m('.container-name', container.name),
+            m('.attached-proxies', [
+                renderSelectProxy(container.cookieStoreId, proxies[0])
+            ]),
+        ])
+    }
+
+    const ContainerListView = {
+        oninit: ContainerListModel.loadAll,
+        view: () => {
+            const items = ContainerListModel.containers.map(renderContainerItem);
+            const defaultContainer = renderContainerItem({
+                cookieStoreId: 'firefox-default',
+                name: '<default container>'
+            })
+            return m('.containers', [...items, defaultContainer])
+        }
+    }
+
+    return ContainerListView
+})()
