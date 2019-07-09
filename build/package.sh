7240bcb5e79a04ca485284dc0becdd9d915fa679
Aleksei Bekh-Ivanov <bekh6ex@users.noreply.github.com>
2019-07-09 18:04:13 +0200

    Add TODO

--- build/package.sh
+++ build/package.sh
@@ -4,6 +4,7 @@ NAME="container-proxy"
 
 VERSION=$(sed -nE 's/^.*"version"[[:space:]]*:[[:space:]]*"([^"]*).*$/\1/p' src/manifest.json)
 
+#TODO Stop if Git is in an unclean state
 mkdir -p dist
 cd src
 zip -r ../dist/${NAME}-v${VERSION}.xpi *
