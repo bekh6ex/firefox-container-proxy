b5db7a7470364846fbd7aec220a7f349deecef96
Aleksei Bekh-Ivanov <bekh6ex@users.noreply.github.com>
2019-07-17 17:09:16 +0200

    Make ProxyListModel nonstatic dependency for ProxyList component

--- src/options/ProxyList.js
+++ src/options/ProxyList.js
@@ -1,45 +1,53 @@
 import m from './lib/mithril.js'
 import {style} from './constants.js'
 
-
-const ProxyListModel = {
-    list: [],
-    loadList: async () => {
-        ProxyListModel.list = await store.getAllProxies()
-        m.redraw()
-    },
-    current: {},
-    delete: async (id) => {
+class ProxyListModel {
+    constructor() {
+        this.list = []
+        this.current = {}
+    }
+    async loadList() {
+        this.list = await store.getAllProxies()
+    }
+    async delete(id) {
         await store.deleteProxyById(id)
-        await ProxyListModel.loadList()
+        await this.loadList()
     }
 }
 
-function renderProxyItem(p) {
-    const text = p.title ? p.title : `${p.host}:${p.port}`;
-    const editButton = m('button.edit[type=button]', {
-        href: '/proxies/' + p.id,
-        oncreate: m.route.link,
-        class:  style.action
-    }, 'edit');
-    const deleteButton = m('button.delete[type=button]', {
-        oncreate: m.route.link,
-        onclick: () => ProxyListModel.delete(p.id),
-        class: style.action
-    }, 'delete');
-    return m('.proxy-list-item', [text, editButton, deleteButton])
-}
-
-
 export class ProxyList {
-    oninit() {
-        ProxyListModel.loadList()        
+    constructor() {
+        this.model = new ProxyListModel()
+    }
+    
+    async oninit() {
+        await this.model.loadList() 
+        m.redraw()
     } 
+    
     view() {
-        const items = ProxyListModel.list.map(renderProxyItem);
+        const items = this.model.list.map(this.renderProxyItem.bind(this));
         const actions = m('.actions', [
             m('a[href=/proxies/new]', {oncreate: m.route.link, class: style.action }, "+")
         ])
         return [...items, actions]
+    }
+
+    renderProxyItem(p) {
+        const text = p.title ? p.title : `${p.host}:${p.port}`;
+        const editButton = m('button.edit[type=button]', {
+            href: '/proxies/' + p.id,
+            oncreate: m.route.link,
+            class:  style.action
+        }, 'edit');
+        const deleteButton = m('button.delete[type=button]', {
+            onclick: async () => {
+                await this.model.delete(p.id)
+                m.redraw()
+            },
+            class: style.action
+        }, 'delete');
+        
+        return m('.proxy-list-item', [text, editButton, deleteButton])
     }
 }
