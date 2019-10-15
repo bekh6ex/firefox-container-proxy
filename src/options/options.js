e63da7573d6a40338b9da223d3164aa0cf0119fa
Aleksei Bekh-Ivanov <bekh6ex@users.noreply.github.com>
2019-10-15 22:05:30 +0200

    Fix code style

--- src/options/options.js
+++ src/options/options.js
@@ -1,27 +1,27 @@
 import { Store } from '../store/Store.js'
 import m from '../lib/mithril.js'
-import {Layout} from './Layout.js'
-import {ContainerListView} from './ContainerListView.js'
-import {ProxyList} from './ProxyList.js'
-import {ProxyForm} from './ProxyForm.js'
+import { Layout } from './Layout.js'
+import { ContainerListView } from './ContainerListView.js'
+import { ProxyList } from './ProxyList.js'
+import { ProxyForm } from './ProxyForm.js'
 
-window.store =  new Store()
+window.store = new Store()
 
 const layout = new Layout()
 const containerListView = new ContainerListView()
-const proxyList = new ProxyList();
+const proxyList = new ProxyList()
 const proxyForm = new ProxyForm()
 
 m.route(document.body, '/containers', {
   '/containers': {
     render: () => m(layout, m(containerListView))
-    },
+  },
   '/proxies': {
     render: () => m(layout, m(proxyList))
-    },
+  },
   '/proxies/:id': {
     render: vnode => m(layout, m(proxyForm, vnode.attrs))
-        }
+  }
 })
 
 document.title = browser.i18n.getMessage('optionPageTitle')
