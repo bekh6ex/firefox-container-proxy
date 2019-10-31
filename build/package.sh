3442baafd50bac6114314c39416c49a53e3f0bc1
Aleksei Bekh-Ivanov <bekh6ex@users.noreply.github.com>
2019-10-31 09:12:48 +0100

    Update version to 0.1.12

--- build/package.sh
+++ build/package.sh
@@ -7,6 +7,7 @@ NAME="container-proxy"
 VERSION=$(sed -nE 's/^.*"version"[[:space:]]*:[[:space:]]*"([^"]*).*$/\1/p' src/manifest.json)
 
 #TODO Stop if Git is in an unclean state
+set -x
 mkdir -p dist
 cd src
 zip -r ../dist/${NAME}-v${VERSION}.xpi *
