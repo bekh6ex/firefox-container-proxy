c7559d7ab2c67d2ddce0cc006377c422724a0f1c
Aleksei Bekh-Ivanov <bekh6ex@users.noreply.github.com>
2021-11-02 23:35:57 +0100

    Fix tests?

--- test/functional/page-objects/OptionsPageObject.js
+++ test/functional/page-objects/OptionsPageObject.js
@@ -27,6 +27,7 @@ class OptionsPageObject extends PageObject {
    */
   async openProxyList () {
     await this.click(this.proxies)
+    await this.pause(1000) // TODO: Waiting for loading
     return this.createPageObject(ProxyListPageObject)
   }
 
@@ -35,6 +36,7 @@ class OptionsPageObject extends PageObject {
    */
   async openAssignProxy () {
     await this.click(this.assign)
+    await this.pause(1000) // TODO: Waiting for loading
     return this.createPageObject(AssignPageObject)
   }
 }
